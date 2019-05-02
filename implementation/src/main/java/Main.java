6593848b1886f316213577d67f7c2264ef063529
Nejc Ribic <nejc.ribic95@gmail.com>
2019-05-02 17:04:44 +0200

    changes in parsing architecture

--- implementation/src/main/java/Main.java
+++ implementation/src/main/java/Main.java
@@ -1,11 +1,14 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import regex.RegEx;
+import sun.plugin.dom.html.HTMLObjectElement;
 import utils.HTMLHelper;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        HTMLHelper.printMe();
-
-
+    public static void main(String[] args) throws Exception{
+        System.out.println("############ Parsing overstock ###########");
+        System.out.println(RegEx.parsePageOverstock(0));
+//        https://stackoverflow.com/questions/6560672/java-regex-to-extract-text-between-tags
     }
 
 }
