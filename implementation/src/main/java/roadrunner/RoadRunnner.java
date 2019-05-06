f5b40fa72fa9533ffb6bf4ec8cb671788a71bf19
Nejc Ribic <nejc.ribic95@gmail.com>
2019-05-06 18:35:20 +0200

    brainstorm of road runner

--- implementation/src/main/java/roadrunner/RoadRunnner.java
+++ implementation/src/main/java/roadrunner/RoadRunnner.java
@@ -1,5 +1,9 @@
 package roadrunner;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import utils.HTMLHelper;
+
 /**
  * Road runner idea. Read two html documents. Create DOM tree for both of them and then
  * walk over the whole tree, and where you find attrs that are not the same, mark as important
@@ -7,4 +11,14 @@ package roadrunner;
  * find position where the next "div" tag is the same.
  */
 public class RoadRunnner {
+    public static void main(String[] args) throws Exception{
+        System.out.println(startRoadRunner(HTMLHelper.getHTMLString("input/overstock.com/jewelry01.html"),
+                HTMLHelper.getHTMLString("input/overstock.com/jewelry01.html")));
+    }
+
+    public static String startRoadRunner(String page1, String page2) {
+        Document document1 = Jsoup.parse(page1);
+        Document document2 = Jsoup.parse(page2);
+        return "test";
+    }
 }
