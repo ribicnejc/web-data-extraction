1a9994b0b97200fffda628547aecc64a688248ec
Nejc Ribic <nejc.ribic95@gmail.com>
2019-05-06 17:38:22 +0200

    brainstorm of road runner

--- implementation/src/main/java/roadrunner/RoadRunnner.java
+++ implementation/src/main/java/roadrunner/RoadRunnner.java
@@ -1,4 +1,10 @@
 package roadrunner;
 
+/**
+ * Road runner idea. Read two html documents. Create DOM tree for both of them and then
+ * walk over the whole tree, and where you find attrs that are not the same, mark as important
+ * data and continue walking. Important thing is that when finding different values, runner must
+ * find position where the next "div" tag is the same.
+ */
 public class RoadRunnner {
 }
